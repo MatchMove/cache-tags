1725c19e8d3af0966792cd1e18a101e872b28b5a
Gian Carlo Val Ebao <gianebao@gmail.com>
2013-09-25 14:19:49 +0800

    added cached tag

--- classes/Cachetag/Core.php
+++ classes/Cachetag/Core.php
@@ -1,23 +1,110 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-abstract class Cachetag_Core {
+class Cachetag_Core {
+    const DEFAULT_CONFIG_PATH ='cachetag';
+    protected static $default_group = 'default';
     
-    static protected $instance = null;
+    protected $_config = null;
+    protected $_tag = null;
     
-    public static instance()
+    /**
+     * sets tag for the token.
+     *
+     * @param  mixed  $group  configurations
+     * return Cache
+     */
+    public function __construct($group = null)
     {
-        self::$instance = 
-    }
-    
-    public function add($id, $tags = array(), $data, $life)
-    {
+        if (empty($group))
+        {
+            $group = Cachetag_Tag::$default_config_group;
+        }
         
+        if (is_string($group))
+        {
+            $config = Kohana::$config->load(self::DEFAULT_CONFIG_PATH)->as_array();
+            
+            // load configuration from kohana config file.
+            if (empty($config[$group]))
+            {
+                throw new Kohana_Exception('Cannot find :class configuration group `:group` on file `:file`',
+                    array(':class' => __CLASS__, ':group' => $group, ':file' => self::DEFAULT_CONFIG_PATH));
+                
+                return false;
+            }
+            
+            $group = $config[$group];
+        }
+        
+        $this->_cache = Cache::instance($group['local']);
+        $this->_tag = new Cachetag_Tag($group['tag']);
     }
     
-    public function get_id($id, $tags = array())
+    /**
+     * sets tag for the token.
+     *
+     * return Cache
+     */
+    public function tags($tags = array())
     {
-        Cachetag_Tag
+        $this->tag()->set($tags);
+        
+        return $this;
     }
     
-
+    /**
+     * get the cache instance being used
+     *
+     * return Cache
+     */
+    public function cache()
+    {
+        return $this->_cache;
+    }
+    
+    /**
+     * get the cache instance being used
+     *
+     * return Cache
+     */
+    public function tag()
+    {
+        return $this->_tag;
+    }
+    
+    /**
+     * create the proper token id
+     *
+     * @param  string  $id  name of the token
+     * return string
+     */
+    public function get_id($id)
+    {
+        return md5($this->tag() . $id);
+    }
+    
+    /**
+     * get the cache token
+     *
+     * @param  string  $id       name of the token
+     * @param  string  $default  return value if token does not exist
+     * return string
+     */
+    public function get($id, $default = NULL)
+    {
+        return $this->cache()->get($this->get_id($id), $default);
+    }
+    
+    /**
+     * get the cache token
+     *
+     * @param  string  $id       name of the token
+     * @param  string  $data  return value if token does not exist
+     * @param  string  $data  return value if token does not exist
+     * return string
+     */
+    public function set($id, $data, $lifetime = NULL)
+    {
+        return $this->cache()->set($this->get_id($id), $data, $lifetime);
+    }
 }
--- classes/Cachetag/Tag/Core.php
+++ classes/Cachetag/Tag/Core.php
@@ -1,15 +1,68 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Cachetag_Tag_Core {
+    const DEFAULT_CONFIG_PATH = 'cachetag';
+    public static $default_config_group = 'default';
+    
+    protected $_tags = array();
+    
     /**
      * Upper limit of the random range
      */
-    protected $key_range = 10000;
-    protected $cache = null;
+    protected $_key_range = 10000;
+    protected $_lifetime  = Date::DAY;
+    protected $_cache = null;
     
-    public function __construct($cache)
+    public function __construct($group)
     {
-        $this->cache = $cache;
+        $this->_cache = Cache::instance($group);
+    }
+    
+    public function __toString()
+    {
+        $this->get($this->_tags);
+    }
+    
+    /**
+     * Lazily sets the value of the tag.
+     * Can be use in tandem with `__toString` magic method
+     *
+     * @param  mixed  $name  name of the tag/s
+     * @return string
+     */
+    public function set($name)
+    {
+        if (!is_array($name))
+        {
+            $name = array($name);
+        }
+        
+        $this->_tags = $name;
+        
+        return $this;
+    }
+
+    /**
+     * Get a cache tag/s
+     *
+     * @param  mixed  $name  name of the tag/s
+     * @return string
+     */
+    public function get($name)
+    {
+        if (!is_array($name))
+        {
+            $name = array($name);
+        }
+        
+        $key = '';
+        
+        foreach ($name as $item)
+        {
+            $key = $this->_get($item);
+        }
+        
+        return $key;
     }
     
     /**
@@ -18,37 +71,47 @@ class Cachetag_Tag_Core {
      * @param  string  $name  name of the tag
      * @return string
      */
-    public function get($name)
+    protected function _get($name)
     {
-        $cache = $this->cache;
+        $cache = $this->_cache;
         $min_key_range = 1;
         
-        if(false === ($value = $cache->get(self::name($name), false)))
+        if(false === ($value = $cache->get(self::_name($name), false)))
         {
-            $value = rand($min_key_range, $this->key_range);
-            $cache->set(self::name($name), $value);
+            $value = rand($min_key_range, $this->_key_range);
+            $cache->set(self::_name($name), $value, $this->_lifetime);
         }
         
         return strtoupper($name) . $value;
     }
     
     /**
-     * Reset the tag
+     * Reset the tag/s
      *
-     * @param  string  $name  name of the tag
+     * @param  mixed  $name  name of the tag
      */
     public function flush($name)
     {
-        $this->cache->increment(self::prefix($name));
+        if (!is_array($name))
+        {
+            $name = array($name);
+        }
+        
+        $key = '';
+        
+        foreach ($name as $item)
+        {
+            $this->_cache->increment(self::_name($item));
+        }
     }
     
     /**
-     * Creates a tagtoken
+     * Creates a token tag
      *
      * @param  string  $name  name of the tag
      * @return string
      */
-    protected static function name($name)
+    protected static function _name($name)
     {
         return 'TAG_' . hash('crc32b', strtoupper($name));
     }
